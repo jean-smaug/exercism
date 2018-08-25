17fcf114dc02d4555ce91335091135e10dc7764c
maxime blanc <maximeblanc.dev@gmail.com>
2018-08-25 15:42:25 +0200

    javascript/leap

---
+++ javascript/leap/leap.js
@@ -0,0 +1,21 @@
+class Year {
+  constructor(year) {
+    this.year = year;
+  }
+
+  isLeap() {
+    if (this.year % 4 !== 0) {
+      return false;
+    }
+    if (this.year % 100 !== 0) {
+      return true;
+    }
+    if (this.year % 400 !== 0) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+export default Year;
