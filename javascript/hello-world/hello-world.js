cacf24bf358853cb7ddca4b9dd1a9cd784fa9183
maxime blanc <maximeblanc.dev@gmail.com>
2018-08-25 15:31:41 +0200

    javascript/hello-world

---
+++ javascript/hello-world/hello-world.js
@@ -0,0 +1,16 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+class HelloWorld {
+  constructor() {
+    this.helloWorld = 'Hello, World!';
+  }
+
+  hello() {
+    return this.helloWorld;
+  }
+}
+
+export default HelloWorld;
