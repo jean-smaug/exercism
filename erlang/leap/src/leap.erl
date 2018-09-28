2238559967a0fb5469bb52e9140c24fa479eca67
Maxime Blanc <maximeblanc.dev@gmail.com>
2018-09-28 13:00:36 +0200

    fixup! erlang/leap

--- erlang/leap/src/leap.erl
+++ erlang/leap/src/leap.erl
@@ -2,7 +2,17 @@
 
 -export([leap_year/1, test_version/0]).
 
+leap_year(Year) ->
+    if
+        Year rem 4 /= 0 -> 
+            false;
+        Year rem 100 /= 0 -> 
+            true; 
+        Year rem 400 /= 0 -> 
+            false; 
+        true -> 
+            true 
+    end.
 
-leap_year(_Year) -> undefined.
 
 test_version() -> 4.
